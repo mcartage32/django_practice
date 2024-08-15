3a6b18cc47244cd09a4b376e598f50addd48a82d
Marcelo Cartagena <mcartage33@gmail.com>
2024-08-15 12:28:55 -0500

    Se generan templates y se agregan por medio del render a las vistas

--- myapp/views.py
+++ myapp/views.py
@@ -4,18 +4,17 @@ from .models import Project, Task
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index page")
+    return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse("About")
+    return render(request, 'about.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def task(request):
+    return render(request, 'tasks.html')
 
 def hello(request, username):
     return HttpResponse("Hello %s" % username)
 
-def projects(request):
-    projects = list(Project.objects.values())
-    return JsonResponse(projects, safe = False)
-
-def task(request, id):
-    task = get_object_or_404(Task, id = id)
-    return HttpResponse("task %s" %task.title)
