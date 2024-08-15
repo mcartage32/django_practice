7c56010402576832cae95e0db8f77ef603dccdb2
Marcelo Cartagena <mcartage33@gmail.com>
2024-08-15 10:59:36 -0500

    Url con parametros y busqueda en modelos con estos

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Task
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("Hello worlhhhd")
+def index(request):
+    return HttpResponse("Index page")
 
 def about(request):
     return HttpResponse("About")
+
+def hello(request, username):
+    return HttpResponse("Hello %s" % username)
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe = False)
+
+def task(request, id):
+    task = get_object_or_404(Task, id = id)
+    return HttpResponse("task %s" %task.title)
